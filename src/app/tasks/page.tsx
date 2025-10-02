52ead50e329cd963b619c417f385d3988c9f4e4c
MohamedAnasBenMim <medanasbenmim888@gmail.com>
2025-10-02 21:28:13 +0100

    project completed

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,9 @@
+async function TaskPage() {
+    const response = await fetch('http://localhost:3000/api/tasks',{
+        cache:"no-store"
+    });
+    const tasks= await response.json();
+  return <div>TaskPage</div>
+}
+
+export default TaskPage
